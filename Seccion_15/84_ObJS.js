b736eeec50712f2cc67a0461a753e4a73cf2d7b5
Quiroz <cristianquiroz2311@gmail.com>
2023-01-12 08:48:19 -0300

    Termine la Seccion 15 ahora vamos por la 16

---
+++ Seccion_15/84_ObJS.js
@@ -0,0 +1,16 @@
+//Objeto: Vacio
+const miPrimerObjeto = {}
+
+//Un objetos es una agrupacion de datos y estos datos hacen sentido entre si
+
+const miObjeto = {
+    unNumero: 5,
+    unString: 'Hola',
+    unaCondicion: true,
+}
+
+//Esto ejecutara todo lo que alla en el objeto
+console.log(miObjeto);
+//Esto ejecutara una parte que nosotros queramos de los datos que estan en el objeto
+console.log(miObjeto.unNumero)
+console.log(miObjeto.unString)
---
+++ Seccion_15/85_Arreglos.js
@@ -0,0 +1,11 @@
+//Arreglos: Los arreglos son como las listas 
+const arrVacio = []
+const arr = [1,2,'Hola']
+//console.log(arr,arrVacio)
+
+//Agregar mas cosas al arreglo
+arr.push(5)
+arrVacio.push('hola')
+console.log(arr,arrVacio)
+//Tambien podemos pasarle variables que ya hemos creado
+/*----------------- */
