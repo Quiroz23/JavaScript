a517d369554abfcc390c73e43eb80f29895fe353
Quiroz <cristianquiroz2311mail.com>
2023-08-07 08:03:37 -0400

    Estudie If

---
+++ Seccion_17/if.js
@@ -0,0 +1,15 @@
+//Control de flujo
+//Aqui evaluaremos las condiciones que queramos en nuestro codigo dependiendo de que hagamos si es correcta la evaluacion lo ejecutara y si es falso se lo saltara
+// Siempre en un if se tiene que escribir un else
+
+const edad = 10
+if (edad > 5 && edad <18) {
+    console.log("Estas en el rango de edad, puedes jugar");
+}
+const edad2 =5 
+if (edad2 > 5 && edad2 <18) {
+    console.log("Estas en el rango de edad, puedes jugar");
+}else{
+    console.log("No estas en el rango de edad, no puedes jugar");
+}
+
